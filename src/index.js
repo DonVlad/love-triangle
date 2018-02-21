b00b05c22447c3e00de8e5a65b463d9611baabb3
DonVlad <boykovdon@gmail.com>
2018-02-21 14:55:36 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,66 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    preferences.unshift(0);
+    var nodeArr = [];
+    var stepsCount = 0;
+
+    function makeStep(step) {
+
+        if (stepsCount < preferences.length) {
+            var nextStep;
+
+            if (nodeArr.indexOf(step) === -1) {
+
+                stepsCount ++;
+                nodeArr.push(step);
+                nextStep = preferences[step];
+                makeStep(nextStep);
+
+            }
+
+            else {
+
+                for (var i = 1; i < preferences.length; i++) {
+
+                    if (nodeArr.indexOf(i) == -1) {
+                        nextStep = i;
+                        break;
+                    }
+
+                }
+                nodeArr.push(step)
+                nodeArr.push('-');
+                makeStep(nextStep);
+            }
+        }
+
+        return;
+    }
+
+    makeStep(1);
+    nodeArr = nodeArr.join(' ');
+    nodeArr = nodeArr.split('-');
+    var count = 0;
+
+    for (var i = 0; i < nodeArr.length; i++) {
+        var node = nodeArr[i].split(' ');
+        // console.log(node);
+
+        for (var j = 0; j < node.length; j++) {
+
+            if (node[j] !== '' && node.lastIndexOf(node[j]) !== -1)  {
+
+                if (node.lastIndexOf(node[j]) - node.indexOf(node[j]) === 3) {
+                        count ++;
+                }
+            }
+        }
+
+    }
+    count = count / 2;
+    // console.log(nodeArr);
+    // console.log(count);
+    return count;
+
 };
